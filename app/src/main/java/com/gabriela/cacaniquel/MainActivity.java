517571577ae1f2ff2cebb10a56e45ad29e76d3d4
victorcals <gabrielamtkb@gmail.com>
2022-09-28 21:04:05 -0300

    oi

--- app/src/main/java/com/gabriela/cacaniquel/MainActivity.java
+++ app/src/main/java/com/gabriela/cacaniquel/MainActivity.java
@@ -3,6 +3,7 @@ package com.gabriela.cacaniquel;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -53,10 +54,15 @@ public class MainActivity extends AppCompatActivity {
 
         sair.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void onClick(View v) {
+                Intent tela = new Intent(getApplicationContext(), Teste.class);
+                startActivity(tela);
             }
         });
+
+
+
+
 
         apostar.setOnClickListener(new View.OnClickListener() {
             @Override
